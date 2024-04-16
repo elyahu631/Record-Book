01214ce682ccf3068b8ecec2ba87ef2dfa29d596
Elyahu Anavi <elyahu631@gmail.com>
2024-04-16 13:41:44 +0300

    Add project files.

---
+++ Record Book/Views/Main.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Record_Book.Models;
+using Record_Book.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Record_Book.Views
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+            MainViewModel mainViewModel = new MainViewModel();
+            this.DataContext = mainViewModel;
+        }
+
+        private void FilterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UserList.Items.Filter = FilterMethod;
+        }
+
+        private bool FilterMethod(object obj)
+        {
+            var user = (User)obj;
+            return user.Name.Contains(FilterTextBox.Text, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
